eb0149ae35c178469e23f859e1ae73aa292f446f
Spl3en <spl3en.contact@gmail.com>
2020-04-07 14:24:49 +0200

    Updates

---
+++ precache-manifest.62892134da88471d6c61c5a73ac6bacc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33d630454cd751ff1422b4051fb5003",
+    "url": "/iconswap-testnet/index.html"
+  },
+  {
+    "revision": "68e035f4f11318d7e564",
+    "url": "/iconswap-testnet/static/css/main.782e864b.chunk.css"
+  },
+  {
+    "revision": "e0c9f8c0b4bac05a16b8",
+    "url": "/iconswap-testnet/static/js/2.eef84456.chunk.js"
+  },
+  {
+    "revision": "4250ce5066147f36ad65d813c23c4d7a",
+    "url": "/iconswap-testnet/static/js/2.eef84456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e035f4f11318d7e564",
+    "url": "/iconswap-testnet/static/js/main.efe13eed.chunk.js"
+  },
+  {
+    "revision": "0ccdd3e64db74f483a0a",
+    "url": "/iconswap-testnet/static/js/runtime-main.448028cb.js"
+  },
+  {
+    "revision": "c2f4c6640a9bbd2204fb511b3d99c112",
+    "url": "/iconswap-testnet/static/media/Account.c2f4c664.svg"
+  },
+  {
+    "revision": "a76f746e467c0b8d038fe54f58018c6c",
+    "url": "/iconswap-testnet/static/media/PowerOff.a76f746e.svg"
+  },
+  {
+    "revision": "e93073b67141cb5836a25711104cff58",
+    "url": "/iconswap-testnet/static/media/Swap.e93073b6.svg"
+  },
+  {
+    "revision": "d221ee9e3d9b6f070be6a3dc3094b924",
+    "url": "/iconswap-testnet/static/media/iconation.d221ee9e.png"
+  },
+  {
+    "revision": "a06390f34e34e8ba2b66f0a61f84630e",
+    "url": "/iconswap-testnet/static/media/listing.a06390f3.svg"
+  },
+  {
+    "revision": "45b9800e89e395d958d76467e99d3bd0",
+    "url": "/iconswap-testnet/static/media/logo.45b9800e.png"
+  }
+]);
